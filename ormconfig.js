ea24f117b768da2a0dae483a316c9d31ef72f5c1
Thiago Puntar <thiagopuntar@gmail.com>
2021-09-17 10:17:55 -0300

    fix ormconfig

--- ormconfig.js
+++ ormconfig.js
@@ -1,14 +1,10 @@
 module.exports = {
-  type: process.env.TYPEORM_CONNECTION,
-  host: process.env.TYPEORM_HOST,
-  port: process.env.TYPEORM_PORT,
-  username: process.env.TYPEORM_USERNAME,
-  password: process.env.TYPEORM_PASSWORD,
-  database: process.env.TYPEORM_DATABASE,
-  entities: [process.env.TYPEORM_ENTITIES],
+  type: process.env.DATABASE_TYPE,
+  host: process.env.DATABASE_HOST,
+  port: process.env.DATABASE_PORT,
+  username: process.env.DATABASE_USER,
+  password: process.env.DATABASE_PASSWORD,
+  database: process.env.DATABASE_NAME,
+  entities: [process.env.DATABASE_ENTITIES],
   synchronize: process.env.SYNC_DATABASE,
-  migrations: ['migration/*.js'],
-  cli: {
-    migrationsDir: 'src/migration',
-  },
 };
