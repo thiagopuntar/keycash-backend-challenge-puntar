dc7e1b688fd683913fec52dc51c6992c4bd2676f
Thiago Puntar <thiagopuntar@gmail.com>
2021-09-17 10:29:52 -0300

    configure swagger

--- src/property/dtos/createProperty.dto.ts
+++ src/property/dtos/createProperty.dto.ts
@@ -1,13 +1,17 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsNotEmpty, IsNumber } from 'class-validator';
 
 export class CreatePropertyDto {
+  @ApiProperty()
   @IsNotEmpty()
   ownerName: string;
 
   @IsNotEmpty()
+  @ApiProperty()
   customerName: string;
 
   @IsNotEmpty()
   @IsNumber()
+  @ApiProperty()
   value: number;
 }
--- src/property/dtos/updateProperty.dto.ts
+++ src/property/dtos/updateProperty.dto.ts
@@ -1,15 +1,25 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsNumber, IsOptional, IsString } from 'class-validator';
 
 export class UpdatePropertyDto {
   @IsString()
   @IsOptional()
+  @ApiProperty({
+    required: false,
+  })
   ownerName?: string;
 
   @IsString()
   @IsOptional()
+  @ApiProperty({
+    required: false,
+  })
   customerName?: string;
 
   @IsNumber()
   @IsOptional()
+  @ApiProperty({
+    required: false,
+  })
   value?: number;
 }
